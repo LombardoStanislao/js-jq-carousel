1e558bfa7b9307a96b7f5dfc7f5ca10d382ea21f
Stanislao <lombardo.stanislao94@xn--lieb-spa>
2020-10-27 17:52:19 +0100

    Problemi con i pallini sotto lo slider

--- main.js
+++ main.js
@@ -5,9 +5,41 @@ $(document).ready(function() {
 
         // Giocando con la classe .active, faccio si che le immagini switchino tra loro
 
-        var imageDisplayed = $('img.active').removeClass('active').next().addClass('active');
+        var imageDisplayed = $('img.active');
 
-        
+        imageDisplayed.removeClass('active');
+
+
+        if (imageDisplayed.hasClass('last')) {
+
+            $('.carousel img:first-of-type').addClass('active');
+        } else {
+            imageDisplayed.next().addClass('active');
+        }
+
+        if ($('.fa-circle').hasClass('fas')) {
+
+            $('.fa-circle').removeClass('fas').next().addClass('far')
+
+        }
+
+
+
+    });
+
+    $('.prev i').click(function() {
+
+        var imageDisplayed = $('img.active');
+
+        imageDisplayed.removeClass('active');
+
+        if (imageDisplayed.hasClass('first')) {
+
+            $('.carousel img:last-of-type').addClass('active');
+
+        } else {
+            imageDisplayed.prev().addClass('active');
+        }
 
     });
 
